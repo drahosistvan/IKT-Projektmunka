56b121e7e5aab6e8707646f0c06c02de00e72b46
Istvan Drahos <drahos.istvan@gmail.com>
2025-04-26 13:08:59 +0200

    fix folder for bgit mv resources/js/pages resources/js/Pagesuilding

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -30,7 +30,7 @@ pinia.use(piniaPluginPersistedState);
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue')),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
